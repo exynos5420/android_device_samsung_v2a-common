c4e3944d1ad84385cb3f0f9936fa36eaf6a4ba9a
Thomas Patry <thompatry@gmail.com>
2023-03-03 03:15:36 +0000

    v2a-common: add universal5420-common to extract scripts
    Change-Id: I592a6c96df16f003386a537b645422e169203543

--- extract-files.sh
+++ extract-files.sh
@@ -1,72 +1,43 @@
 #!/bin/bash
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-License-Identifier: Apache-2.0
 #
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
+function blob_fixup() {
+    case "${1}" in
+        etc/wifi/nvram_mfg.txt_4339_a0)
+            sed -i 's|ccode=ALL|ccode=00|g' "${2}"
+            sed -i 's|regrev=0|regrev=6|g' "${2}"
+            ;;
+        etc/wifi/nvram_mfg.txt_4354_a0)
+            sed -i 's|ccode=ALL|ccode=00|g' "${2}"
+            sed -i 's|regrev=0|regrev=6|g' "${2}"
+            ;;
+        etc/wifi/nvram_mfg.txt_4354_a1)
+            sed -i 's|ccode=ALL|ccode=00|g' "${2}"
+            sed -i 's|regrev=0|regrev=6|g' "${2}"
+            ;;
+        etc/wifi/nvram_net.txt_4339_a0)
+            sed -i 's|ccode=GB|ccode=00|g' "${2}"
+            ;;
+        etc/wifi/nvram_net.txt_4354_a0)
+            sed -i 's|ccode=GB|ccode=00|g' "${2}"
+            ;;
+        etc/wifi/nvram_net.txt_4354_a1)
+            sed -i 's|ccode=GB|ccode=00|g' "${2}"
+            ;;
+    esac
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
 
 set -e
 
-VENDOR=samsung
-DEVICE_COMMON=v2a-common
-
-# Load extractutils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-LINEAGE_ROOT="$MY_DIR"/../../..
-
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
-fi
-
-# Initialize the helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
-
-extract "$MY_DIR"/proprietary-files.txt "$SRC"
-
-# Fix proprietary blobs
-BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
-
-# nvram_mfg.txt
-sed -i 's|ccode=ALL|ccode=00|g' $BLOB_ROOT/etc/wifi/nvram_mfg.txt_4339_a0
-sed -i 's|regrev=0|regrev=6|g' $BLOB_ROOT/etc/wifi/nvram_mfg.txt_4339_a0
-sed -i 's|ccode=ALL|ccode=00|g' $BLOB_ROOT/etc/wifi/nvram_mfg.txt_4354_a0
-sed -i 's|regrev=0|regrev=6|g' $BLOB_ROOT/etc/wifi/nvram_mfg.txt_4354_a0
-sed -i 's|ccode=ALL|ccode=00|g' $BLOB_ROOT/etc/wifi/nvram_mfg.txt_4354_a1
-sed -i 's|regrev=0|regrev=6|g' $BLOB_ROOT/etc/wifi/nvram_mfg.txt_4354_a1
-
-# nvram_net.txt
-sed -i 's|ccode=GB|ccode=00|g' $BLOB_ROOT/etc/wifi/nvram_net.txt_4339_a0
-sed -i 's|ccode=GB|ccode=00|g' $BLOB_ROOT/etc/wifi/nvram_net.txt_4354_a0
-sed -i 's|ccode=GB|ccode=00|g' $BLOB_ROOT/etc/wifi/nvram_net.txt_4354_a1
-
-"${MY_DIR}"/setup-makefiles.sh
+"./../../${VENDOR}/${DEVICE_UNIVERSAL_COMMON}/extract-files.sh" "$@"
